f1528853b47312580fb716d92c2a624cb3f6dfe3
Ignacio <tarda@FE5>
2017-05-24 16:25:09 +0200

    formulario con una base primitiva de un reproductor de archivos .wav y .mp3

---
+++ EsplaiMusic/EsplaiMusic/ReproductorIgnacio.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace EsplaiMusic
+{
+    partial class ReproductorIgnacio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReproductorIgnacio));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.play = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(291, 46);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "Buscar";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(42, 99);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(324, 184);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(421, 36);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 23);
+            this.play.TabIndex = 3;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(421, 79);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 23);
+            this.pause.TabIndex = 4;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(421, 121);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 5;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // ReproductorIgnacio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 287);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ReproductorIgnacio";
+            this.Text = "ReproductorIgnacio";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button stop;
+    }
+}
---
+++ EsplaiMusic/EsplaiMusic/ReproductorIgnacio.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Librerias Windows Media Player
+
+// para archivos WAV
+using System.Media;
+
+// para archivos mp3
+using WMPLib;
+
+namespace EsplaiMusic
+{
+    public partial class ReproductorIgnacio : Form
+    {
+        //MP3
+        private WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+        //WAV
+        private SoundPlayer sp = new SoundPlayer();
+
+        public ReproductorIgnacio()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            browseMusic();
+        }
+
+        private void browseMusic()
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+
+            if (opf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string rutaCancion = opf.FileName;
+                int index = rutaCancion.IndexOf(".");
+                string tipoCancion = rutaCancion.Substring(index + 1, 3);
+
+                //ruta del archivo en el textbox de prueba
+                textBox1.Text = rutaCancion;
+
+                if (tipoCancion.Equals("mp3"))
+                {
+                    reproducirMP3(rutaCancion);
+                }
+                else if (tipoCancion.Equals("wav"))
+                {
+                    reproducirWAV(rutaCancion);
+                }
+                else
+                {
+                    MessageBox.Show("Archivo seleccionado con formato no válido!");
+                }
+            }
+        }
+
+        private void reproducirMP3(string rutaCancion)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            wplayer.URL = rutaCancion;
+            wplayer.controls.play();
+        }
+
+        private void reproducirWAV(string rutaCancion)
+        {
+            sp.SoundLocation = rutaCancion;
+            sp.Play();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            stopMusic();
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.play();
+            sp.Play();
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            pauseMusic();
+        }
+
+        private void stopMusic()
+        {
+            wplayer.controls.stop();
+            sp.Stop();
+        }
+
+        private void pauseMusic()
+        {
+            wplayer.controls.pause();
+        }
+    }
+}
