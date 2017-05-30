36410010c69f1978b08ab3123a90d21dfd5a6775
Sam_Yu <ayufjk123@gmail.com>
2017-05-30 17:59:07 +0200

    Resize

--- ReproductorMusica/Form1.Designer.cs
+++ ReproductorMusica/Form1.Designer.cs
@@ -50,10 +50,9 @@
             // browse
             // 
             this.browse.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.browse.Location = new System.Drawing.Point(122, 42);
-            this.browse.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.browse.Location = new System.Drawing.Point(61, 22);
             this.browse.Name = "browse";
-            this.browse.Size = new System.Drawing.Size(190, 44);
+            this.browse.Size = new System.Drawing.Size(95, 23);
             this.browse.TabIndex = 0;
             this.browse.Text = "Elegir canción";
             this.browse.UseVisualStyleBackColor = true;
@@ -67,23 +66,22 @@
             // 
             this.play.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.play.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.play.Location = new System.Drawing.Point(684, 854);
-            this.play.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.play.Location = new System.Drawing.Point(342, 444);
             this.play.Name = "play";
-            this.play.Size = new System.Drawing.Size(150, 44);
+            this.play.Size = new System.Drawing.Size(75, 23);
             this.play.TabIndex = 1;
             this.play.Text = "Play";
             this.play.UseVisualStyleBackColor = true;
+            this.play.Visible = false;
             this.play.Click += new System.EventHandler(this.play_Click);
             // 
             // stop
             // 
             this.stop.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.stop.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.stop.Location = new System.Drawing.Point(320, 854);
-            this.stop.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.stop.Location = new System.Drawing.Point(160, 444);
             this.stop.Name = "stop";
-            this.stop.Size = new System.Drawing.Size(150, 44);
+            this.stop.Size = new System.Drawing.Size(75, 23);
             this.stop.TabIndex = 2;
             this.stop.Text = "Stop";
             this.stop.UseVisualStyleBackColor = true;
@@ -93,24 +91,21 @@
             // 
             this.pause.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.pause.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pause.Location = new System.Drawing.Point(684, 852);
-            this.pause.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pause.Location = new System.Drawing.Point(342, 443);
             this.pause.Name = "pause";
-            this.pause.Size = new System.Drawing.Size(150, 44);
+            this.pause.Size = new System.Drawing.Size(75, 23);
             this.pause.TabIndex = 3;
             this.pause.Text = "Pause";
             this.pause.UseVisualStyleBackColor = true;
-            this.pause.Visible = false;
             this.pause.Click += new System.EventHandler(this.pause_Click);
             // 
             // siguiente
             // 
             this.siguiente.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.siguiente.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.siguiente.Location = new System.Drawing.Point(864, 854);
-            this.siguiente.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.siguiente.Location = new System.Drawing.Point(432, 444);
             this.siguiente.Name = "siguiente";
-            this.siguiente.Size = new System.Drawing.Size(150, 44);
+            this.siguiente.Size = new System.Drawing.Size(75, 23);
             this.siguiente.TabIndex = 4;
             this.siguiente.Text = "Siguiente";
             this.siguiente.UseVisualStyleBackColor = true;
@@ -118,24 +113,22 @@
             // 
             // lista
             // 
-            this.lista.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lista.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lista.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lista.FormattingEnabled = true;
-            this.lista.ItemHeight = 25;
-            this.lista.Location = new System.Drawing.Point(1648, 42);
-            this.lista.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lista.Location = new System.Drawing.Point(824, 22);
             this.lista.Name = "lista";
-            this.lista.Size = new System.Drawing.Size(686, 904);
+            this.lista.Size = new System.Drawing.Size(345, 472);
             this.lista.TabIndex = 6;
             // 
             // anterior
             // 
             this.anterior.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.anterior.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.anterior.Location = new System.Drawing.Point(500, 854);
-            this.anterior.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.anterior.Location = new System.Drawing.Point(250, 444);
             this.anterior.Name = "anterior";
-            this.anterior.Size = new System.Drawing.Size(150, 44);
+            this.anterior.Size = new System.Drawing.Size(75, 23);
             this.anterior.TabIndex = 7;
             this.anterior.Text = "Anterior";
             this.anterior.UseVisualStyleBackColor = true;
@@ -145,10 +138,9 @@
             // 
             this.mute.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.mute.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.mute.Location = new System.Drawing.Point(1086, 854);
-            this.mute.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.mute.Location = new System.Drawing.Point(543, 444);
             this.mute.Name = "mute";
-            this.mute.Size = new System.Drawing.Size(150, 44);
+            this.mute.Size = new System.Drawing.Size(75, 23);
             this.mute.TabIndex = 8;
             this.mute.Text = "Mute";
             this.mute.UseVisualStyleBackColor = true;
@@ -158,10 +150,9 @@
             // 
             this.unmute.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.unmute.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.unmute.Location = new System.Drawing.Point(1086, 852);
-            this.unmute.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.unmute.Location = new System.Drawing.Point(543, 443);
             this.unmute.Name = "unmute";
-            this.unmute.Size = new System.Drawing.Size(150, 44);
+            this.unmute.Size = new System.Drawing.Size(75, 23);
             this.unmute.TabIndex = 9;
             this.unmute.Text = "Unmute";
             this.unmute.UseVisualStyleBackColor = true;
@@ -177,12 +168,11 @@
             this.volumen.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.volumen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(125)))), ((int)(((byte)(123)))));
             this.volumen.IndentHeight = 6;
-            this.volumen.Location = new System.Drawing.Point(1336, 844);
-            this.volumen.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.volumen.Location = new System.Drawing.Point(668, 439);
             this.volumen.Maximum = 100;
             this.volumen.Minimum = 0;
             this.volumen.Name = "volumen";
-            this.volumen.Size = new System.Drawing.Size(218, 28);
+            this.volumen.Size = new System.Drawing.Size(109, 28);
             this.volumen.TabIndex = 12;
             this.volumen.TextTickStyle = System.Windows.Forms.TickStyle.None;
             this.volumen.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(146)))), ((int)(((byte)(148)))));
@@ -201,10 +191,9 @@
             this.norepeat.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.norepeat.Cursor = System.Windows.Forms.Cursors.Hand;
             this.norepeat.Image = ((System.Drawing.Image)(resources.GetObject("norepeat.Image")));
-            this.norepeat.Location = new System.Drawing.Point(208, 844);
-            this.norepeat.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.norepeat.Location = new System.Drawing.Point(104, 439);
             this.norepeat.Name = "norepeat";
-            this.norepeat.Size = new System.Drawing.Size(64, 62);
+            this.norepeat.Size = new System.Drawing.Size(32, 32);
             this.norepeat.TabIndex = 13;
             this.norepeat.TabStop = false;
             this.norepeat.Click += new System.EventHandler(this.unrepeat_Click);
@@ -214,10 +203,9 @@
             this.repeat.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.repeat.Cursor = System.Windows.Forms.Cursors.Hand;
             this.repeat.Image = global::ReproductorMusica.Properties.Resources.Play_Mode_Repeat_All_Hot_icon;
-            this.repeat.Location = new System.Drawing.Point(208, 844);
-            this.repeat.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.repeat.Location = new System.Drawing.Point(104, 439);
             this.repeat.Name = "repeat";
-            this.repeat.Size = new System.Drawing.Size(64, 62);
+            this.repeat.Size = new System.Drawing.Size(32, 32);
             this.repeat.TabIndex = 14;
             this.repeat.TabStop = false;
             this.repeat.Visible = false;
@@ -232,12 +220,11 @@
             this.duracionCancion.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.duracionCancion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(125)))), ((int)(((byte)(123)))));
             this.duracionCancion.IndentHeight = 6;
-            this.duracionCancion.Location = new System.Drawing.Point(208, 637);
-            this.duracionCancion.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.duracionCancion.Location = new System.Drawing.Point(104, 331);
             this.duracionCancion.Maximum = 10;
             this.duracionCancion.Minimum = 0;
             this.duracionCancion.Name = "duracionCancion";
-            this.duracionCancion.Size = new System.Drawing.Size(1238, 28);
+            this.duracionCancion.Size = new System.Drawing.Size(619, 28);
             this.duracionCancion.TabIndex = 15;
             this.duracionCancion.TextTickStyle = System.Windows.Forms.TickStyle.None;
             this.duracionCancion.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(146)))), ((int)(((byte)(148)))));
@@ -252,9 +239,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2362, 983);
+            this.ClientSize = new System.Drawing.Size(1181, 511);
             this.Controls.Add(this.duracionCancion);
             this.Controls.Add(this.repeat);
             this.Controls.Add(this.norepeat);
@@ -269,9 +256,7 @@
             this.Controls.Add(this.play);
             this.Controls.Add(this.browse);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
-            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.norepeat)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repeat)).EndInit();
